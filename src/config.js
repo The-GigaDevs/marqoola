1ebf552460970a1d4a31c43cf70d3d1fee1ced26
byrongamatos <xasiklas@gmail.com>
2023-06-24 22:21:47 +0200

    applied TomatoGrotesk-Regular as the default font

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ export const HORIZONTAL_MAX_ITEM = 6;
 const config = {
     layout: LAYOUT_CONST.VERTICAL_LAYOUT, // vertical, horizontal
     drawerType: LAYOUT_CONST.DEFAULT_DRAWER, // default, mini-drawer
-    fontFamily: `'Roboto', sans-serif`,
+    fontFamily: `'TomatoGrotesk', sans-serif`,
     borderRadius: 8,
     outlinedFilled: true,
     navType: 'light', // light, dark
