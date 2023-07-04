bc5909e2c27bee59a75695188612a47fb9219149
byrongamatos <xasiklas@gmail.com>
2023-07-04 22:17:54 +0200

    update roadmap with sections

--- src/views/application/roadmap/index.js
+++ src/views/application/roadmap/index.js
@@ -26,7 +26,8 @@ import {
 } from '@mui/material';
 import RoadmapCard from 'ui-component/cards/RoadmapCard';
 import MonetizationOnTwoToneIcon from '@mui/icons-material/MonetizationOnTwoTone';
-import { Icon360 } from '@tabler/icons';
+import { Icon360, Icon3dCubeSphere, Icon123 } from '@tabler/icons';
+import SubCard from 'ui-component/cards/SubCard';
 
 
 const Roadmap = () => {
@@ -35,33 +36,62 @@ const Roadmap = () => {
     return (
         <MainCard title="Roadmap" content={false}>
             <CardContent>
-            <Grid container spacing={3}>
-            <Grid item xs={5} >
-                <RoadmapCard
-                    secondary="Create your own risk categories"
-                    content="Add/Modify/Delete"
-                    iconPrimary={MonetizationOnTwoToneIcon}
-                    color={theme.palette.secondary.main}
-                    title="Create Risk Categories"
-                />
-            </Grid>
-            <Grid item xs={5}>
-                <RoadmapCard
-                    secondary="Create your own risks"
-                    content="Add/Modify/Delete"
-                    iconPrimary={Icon360}
-                    color={theme.palette.primary.main}
-                    title="Create Risks"
-                />
-            </Grid>
-            </Grid>
-                
+                <Grid container spacing={3} >
+                    <Grid item xs={12}  >
+                        <SubCard title="Now"  >
+                            <Grid container spacing={3}>
+                                <Grid item xs={5} >
+                                    <RoadmapCard
+                                        secondary="Create your own risk categories"
+                                        content="Add/Modify/Delete"
+                                        iconPrimary={MonetizationOnTwoToneIcon}
+                                        color={theme.palette.secondary.main}
+                                        title="Create Risk Categories"
+                                    />
+                                </Grid>
+                                <Grid item xs={5}>
+                                    <RoadmapCard
+                                        secondary="Create your own risks"
+                                        content="Add/Modify/Delete"
+                                        iconPrimary={Icon360}
+                                        color={theme.palette.primary.main}
+                                        title="Create Risks"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </SubCard>
+                        <br />
+                    
+                        <SubCard title="Later">
+                            <Grid container spacing={3}>
+                                <Grid item xs={5} >
+                                    <RoadmapCard
+                                        secondary="Create your own assets"
+                                        content="Add/Modify/Delete"
+                                        iconPrimary={Icon3dCubeSphere}
+                                        color={theme.palette.orange.dark}
+                                        title="Create Assets"
+                                    />
+                                </Grid>
+                                <Grid item xs={5}>
+                                    <RoadmapCard
+                                        secondary="Create your own control tests"
+                                        content="Add/Modify/Delete"
+                                        iconPrimary={Icon123}
+                                        color={theme.palette.warning.dark}
+                                        title="Create Control Tests"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </SubCard>
+                    </Grid>
+                </Grid>
             </CardContent>
 
-           
-            
+
+
         </MainCard>
-        
+
     );
 };
 
