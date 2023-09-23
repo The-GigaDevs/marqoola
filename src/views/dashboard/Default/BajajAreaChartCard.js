443660a983a79b862937a55d51e23b0be975520c
byrongamatos <xasiklas@gmail.com>
2023-09-23 20:45:03 +0200

    data for dashboard

--- src/views/dashboard/Default/BajajAreaChartCard.js
+++ src/views/dashboard/Default/BajajAreaChartCard.js
@@ -43,7 +43,7 @@ const BajajAreaChartCard = () => {
                         </Grid>
                         <Grid item>
                             <Typography variant="h4" sx={{ color: theme.palette.grey[800] }}>
-                                $1839.00
+                                $302650.00
                             </Typography>
                         </Grid>
                     </Grid>
